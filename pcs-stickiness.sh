e3495f5e654da0bff1b89173daba3c28c279a610
urusha <urusha.v1.0@gmail.com>
2014-10-05 14:15:29 +0400

    Update pcs-stickiness.sh

--- pcs-stickiness.sh
+++ pcs-stickiness.sh
@@ -5,18 +5,25 @@ TIMEOUT="120"
 # resource-stickiness
 RS="100"
 
+if [ "$1" = "-f" ]; then
+    RSF="YES"
+    shift
+else
+    RSF="NO"
+fi
+
 if echo "$1" | grep -q '^[0-9]\+$' && [ "$1" -gt 0 ]; then
     TIMEOUT="$1"
 elif [ -n "$1" ]; then
-    echo "Usage: `basename $0` [timeout]"
+    echo "Usage: `basename $0` [-f] [timeout]"
     exit 1
 fi
 
-if pcs status | grep -q 'Stopped'; then
+if [ "$RSF" != "YES" ] && pcs status | grep -q "Stopped"; then
     echo "Cluster has 'Stopped' resources. Exiting..."
     exit 1
-else
-    pcs resource defaults resource-stickiness="0"
-    sleep "$TIMEOUT"
-    pcs resource defaults resource-stickiness="$RS"
 fi
+
+pcs resource defaults resource-stickiness="0"
+sleep "$TIMEOUT"
+pcs resource defaults resource-stickiness="$RS"
