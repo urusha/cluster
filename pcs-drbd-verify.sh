0ed5aab3ee78401f4126feccb2e6428031d7a0ed
urusha <urusha.v1.0@gmail.com>
2015-07-09 14:38:21 +0300

    Create pcs-drbd-verify.sh

---
+++ pcs-drbd-verify.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+export LANG=C
+
+if crm_mon -1n | grep -qi 'offline'; then
+    echo "Cluster has offline node(s). Exiting"
+    exit 1
+fi
+
+VERIFYDATE=`date +"%Y-%m-%d %H:%M:%S"`
+drbdadm verify all
+sleep 2
+
+while : ; do
+    grep -q 'Verify' /proc/drbd || break
+    sleep 10
+done
+
+if journalctl --since="$VERIFYDATE" | grep "Online verify found"; then
+    echo "DRBD resync required. Restarting pacemaker..."
+else
+    exit 0
+fi
+
+systemctl stop pacemaker
+sleep 1
+systemctl start pacemaker
+
+echo "Pacemaker restarted"
